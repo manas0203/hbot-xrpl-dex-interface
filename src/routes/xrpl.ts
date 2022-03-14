97fef8ec690148a6404029f6d4bf682c64d0a632
manas0203 <manas@4ecap.com>
2022-03-15 01:54:17 +0800

    renamed and added xrpl

---
+++ src/routes/xrpl.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import {
+  latency,
+  reportConnectionError,
+  statusMessages,
+} from '../services/utils';
+import { logger } from '../services/logger';
+
+
+// Need to define all API endpoints here
+// /balances
+// /start
+// /prices
+// /trade
+
+export default router;
